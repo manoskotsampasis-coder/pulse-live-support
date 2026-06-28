d4eae9bcae0afc4274936d068d94421e3aceb9fd
manoskotsampasis-coder <manoskotsampasis@gmail.com>
2026-06-28 03:55:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
+import os
 from discord import app_commands
 from discord.ext import commands
+
+# ... υπόλοιπος κώδικας
 
 intents = discord.Intents.default()
 intents.message_content = True
